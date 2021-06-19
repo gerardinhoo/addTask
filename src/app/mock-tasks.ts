862ec648f8d27bac35aabe0f9de6023edff74ba7
Gerard Eklu <gerard_eklu@homedepot.com>
2021-06-19 09:43:59 -0400

    Completed the task tracker app in Angular

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Soccer Game',
+    day: '6th June 2021 at 1:00pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'On a date',
+    day: '10th June 2021 at 9:30pm',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: 'Reading',
+    day: '2th July 2021 at 5:00pm',
+    reminder: false
+  }
+]
